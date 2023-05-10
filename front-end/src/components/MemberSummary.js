cb2bcb98eff23e5e5961af7ddc76c6f7a096c69e
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-11 01:09:41 +0800

    added

--- front-end/src/components/MemberSummary.js
+++ front-end/src/components/MemberSummary.js
@@ -2,30 +2,27 @@ import {Paper} from "@mui/material";
 import {memberSummary} from "@modules/utils/config";
 import PaperDashboard from "@modules/components/PaperDashboard";
 import Typography from "@mui/material/Typography";
+import ResponsiveAppBar from "@modules/components/layouts/ResponsiveAppBar";
+import BasicComponent from "@modules/components/layouts/BasicComponent";
 
 export default function MemberSummary(){
     return(
         <Paper  style={{padding:"1rem"}} elevation={24}>
             <Typography variant="h6" display="block" gutterBottom> Member's summary</Typography>
 
-            <div style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-                flexWrap: "wrap",
-                gap: "10px",
-                margin: "10px",
-            }}>
-                {memberSummary.map(summary => (
-                    <PaperDashboard
-                        key={summary.name}
-                        color={summary.color}
-                        name={summary.name}
-                        icon={summary.icon}
-                        value={summary.value}
-                        tooltip={summary.tooltip} />
-                ))}
-            </div>
+           <BasicComponent>
+               {memberSummary.map(summary => (
+                   <PaperDashboard
+                       key={summary.name}
+                       color={summary.color}
+                       name={summary.name}
+                       icon={summary.icon}
+                       value={summary.value}
+                       tooltip={summary.tooltip} />
+               ))}
+           </BasicComponent>
+
+
         </Paper>
     )
 }
