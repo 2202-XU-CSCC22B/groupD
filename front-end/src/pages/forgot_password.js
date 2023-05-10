2c5ca69e7c80dcf38b546a7e4931d732a4668f23
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-11 00:53:15 +0800

    Added layouts

--- front-end/src/pages/forgot_password.js
+++ front-end/src/pages/forgot_password.js
@@ -1,11 +1,19 @@
 import {Container, Typography} from "@mui/material";
 import Link from "next/link";
+import ResponsiveAppBar from "@modules/components/layouts/ResponsiveAppBar";
+import Head from "next/head";
 
 export default function forgotPassword(){
     return (
-        <Container sx={{display: "flex", flexDirection: "column"}}>
-            <Typography> Forgot Password here</Typography>
-            <Link href={"/"}> <Typography variant={"h3"}> Go back to Landing Page!</Typography></Link>
-        </Container>
+        <ResponsiveAppBar>
+            <Head>
+                <title>Password Reset</title>
+            </Head>
+            <Container sx={{display: "flex", flexDirection: "column"}}>
+                <Typography> Forgot Password here</Typography>
+                <Link href={"/"}> <Typography variant={"h3"}> Go back to Landing Page!</Typography></Link>
+            </Container>
+        </ResponsiveAppBar>
+
     )
 }
--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,14 +1,22 @@
 import {Container, Paper, Typography} from "@mui/material";
 import Link from "next/link";
+import ResponsiveAppBar from "@modules/components/layouts/ResponsiveAppBar";
+import Head from "next/head";
 
 export default function LoginPage(){
     return (
+        <ResponsiveAppBar>
+            <Head>
+                <title>Login</title>
+            </Head>
         <Paper>
             Log in page here!
-            <Container sx={{display: "flex", flexDirection: "column"}}>
-                <Link href={"/forgot_password"}> <Typography variant={"h3"}> Forgot password button</Typography></Link>
-                <Link href={"/dashboard"}> <Typography variant={"h3"}> Proceed to home!</Typography></Link>
-            </Container>
+        <Container sx={{display: "flex", flexDirection: "column"}}>
+            <Link href={"/forgot_password"}> <Typography variant={"h3"}> Forgot password button</Typography></Link>
+            <Link href={"/dashboard"}> <Typography variant={"h3"}> Proceed to home!</Typography></Link>
+        </Container>
         </Paper>
+        </ResponsiveAppBar>
+
     )
 }
