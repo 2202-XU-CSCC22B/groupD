6ebc4edcf1cb487ed2a4358e188a4f5c81e8e099
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-15 19:01:23 +0800

    Fix my commits

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,31 +1,22 @@
-import { Button, Container, Paper, TextField, Typography } from "@mui/material";
+import {Container, Paper, Typography} from "@mui/material";
 import Link from "next/link";
 import ResponsiveAppBar from "@modules/components/layouts/ResponsiveAppBar";
 import Head from "next/head";
 
-export default function LoginPage() {
-  return (
-    <ResponsiveAppBar>
-      <Head>
-        <title>Login</title>
-      </Head>
-      <Paper sx={{ bgcolor: "#f2f2f2", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", p: 4 }}>
-          <Typography variant="h4" sx={{ color: "#404040", marginBottom: 4 }}>Welcome to Unscathed Fitness Gym!</Typography>
-          <TextField label="Email" type="email" variant="outlined" sx={{ width: "100%", marginBottom: 2 }} />
-          <TextField label="Password" type="password" variant="outlined" sx={{ width: "100%", marginBottom: 4 }} />
-          <Link href={"/dashboard"}>
-             <Button variant="contained" sx={{ bgcolor: "#404040", color: "#fff", marginBottom: 2 }}>Log In</Button>
-            </Link>
-          <Typography variant="subtitle1" sx={{ color: "#737373", marginBottom: 2 }}>Don't have an account yet?</Typography>
-          <Link href={"/signup"} passHref>
-            <Button variant="outlined" sx={{ bgcolor: "#fff", color: "#404040" }}>Sign Up</Button>
-          </Link>
-          <Link href={"/forgot_password"} passHref>
-            <Typography variant="subtitle1" sx={{ color: "#737373", marginTop: 4 }}>Forgot your password?</Typography>
-          </Link>
+export default function LoginPage(){
+    return (
+        <ResponsiveAppBar>
+            <Head>
+                <title>Login</title>
+            </Head>
+        <Paper>
+            Log in page here!
+        <Container sx={{display: "flex", flexDirection: "column"}}>
+            <Link href={"/forgot_password"}> <Typography variant={"h3"}> Forgot password button</Typography></Link>
+            <Link href={"/dashboard"}> <Typography variant={"h3"}> Proceed to home!</Typography></Link>
         </Container>
-      </Paper>
-    </ResponsiveAppBar>
-  );
+        </Paper>
+        </ResponsiveAppBar>
+
+    )
 }
