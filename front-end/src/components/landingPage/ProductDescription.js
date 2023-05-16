f07477e6f726755feb36840b15b58b1ad0df535c
Scott Baban <200431995@my.xu.edu.ph>
2023-05-16 23:37:18 +0800

    Centered Icons in 'about' (Landing Page)

--- front-end/src/components/landingPage/ProductDescription.js
+++ front-end/src/components/landingPage/ProductDescription.js
@@ -1,38 +1,57 @@
-import {Box, Button, Icon, Paper, Typography} from "@mui/material";
+import { Box, Button, Icon, Paper, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 import * as React from "react";
 import LearnMoreModal from "@modules/components/landingPage/LearnMoreModal";
+import { styled } from "@mui/system";
+export default function ProductDescription(props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    console.log("Open");
+    setOpen(!open);
+  };
 
-;
-export default function ProductDescription(props){
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () =>{
-        console.log("Open")
-        setOpen(!open)
+  const CenteredIcon = styled("div")`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 2rem;
+  `;
 
-    };
-
-    return(
-        <Paper sx={{ padding: "1rem", textAlign: "center", backgroundColor: "#fff", height: "100%" }} elevation={12}>
-            <Box style={{ display: "flex", flexDirection: "column" }}>
-                <Typography variant="h5" sx={{ marginBottom: 2 }}>{props.title}</Typography>
-                <Icon>
-                    {props.icon}
-                </Icon>
-                <Typography variant="subtitle1" sx={{ marginBottom: 2 }}>{props.description}</Typography>
-                <Button variant="contained" sx={{ backgroundColor: "#404040", color: "#fff" }} onClick={handleOpen}>Learn More</Button>
-                <LearnMoreModal open={open}
-                                onClose={()=> setOpen(!open)}/>
-            </Box>
-
-        </Paper>
-    )
+  return (
+    <Paper
+      sx={{
+        padding: "1rem",
+        textAlign: "center",
+        backgroundColor: "#fff",
+        height: "100%",
+      }}
+      elevation={12}
+    >
+      <Box style={{ display: "flex", flexDirection: "column" }}>
+        <Typography variant="h5" sx={{ marginBottom: 2 }}>
+          {props.title}
+        </Typography>
+        <CenteredIcon>
+          <Icon>{props.icon}</Icon>
+        </CenteredIcon>
+        <Typography variant="subtitle1" sx={{ marginBottom: 2 }}>
+          {props.description}
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#404040", color: "#fff" }}
+          onClick={handleOpen}
+        >
+          Learn More
+        </Button>
+        <LearnMoreModal open={open} onClose={() => setOpen(!open)} />
+      </Box>
+    </Paper>
+  );
 }
 
-
-ProductDescription.propTypes={
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    function: PropTypes.func.isRequired,
-    icon: PropTypes.element,
-}
+ProductDescription.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  function: PropTypes.func.isRequired,
+  icon: PropTypes.element,
+};
