20f74cee8cb9b243f4dc048aca4b669db0e1bcd7
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-17 01:21:17 +0300

    Updated register form

--- front-end/src/pages/register.js
+++ front-end/src/pages/register.js
@@ -7,7 +7,7 @@ const RegisterPage = () => {
   return (
     <div className=" bg-white">
       <ResponsiveAppBar />
-      <div className=" bg-white container !max-w-5xl mx-auto px-4 py-8 lg:px-0">
+      <div className=" bg-white container !max-w-6xl mx-auto px-4 py-8 lg:px-0">
         <div className=" flex flex-col md:flex-row gap-6 md:gap-12">
           <RegisterHeader />
           <RegisterForm />
