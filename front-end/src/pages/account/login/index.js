b900f874f05a5ea544f78a512707bd15983d3055
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-18 21:24:34 +0300

    Updated login page

--- front-end/src/pages/account/login/index.js
+++ front-end/src/pages/account/login/index.js
@@ -18,7 +18,7 @@ export default function LoginPage() {
     <>
       <MyContainer className="pt-[87px] text-black min-h-screen ">
         <div className="grid h-[calc(100vh-87px)] place-items-center calc w-full px-4 lg:px-0">
-          <div className="flex flex-col gap-2 w-full max-w-[450px] min-[520px]:px-8 min-[520px]:py-12 sm:w-[450px] border border-gray-300 shadow px-4 py-8">
+          <div className="flex flex-col gap-2 w-full max-w-[450px] min-[520px]:px-8 min-[520px]:py-12 sm:w-[450px] border border-gray-300 shadow px-4 py-8 bg-white rounded-lg">
             <h1 className="text-3xl font-medium pb-5 text-left w-full">
               Login
             </h1>
@@ -44,7 +44,7 @@ export default function LoginPage() {
                 passHref
                 style={{ textDecoration: "none", color: "black" }}
               >
-                <button class="rounded-md w-full bg-gray-800 text-slate-50 font-medium tracking-widest drop-shadow-lg py-3 px-6 uppercase text-base tex -translate-y-2 ">
+                <button className="rounded-md w-full bg-gray-800 text-slate-50 font-medium tracking-widest drop-shadow-lg py-3 px-6 uppercase text-base -translate-y-2  hover:bg-gray-700 duration-300 transition-all ease-in-out">
                   Login
                 </button>
               </Link>
