5d39c6d3e85a6a12111ce553185834d88f2e9048
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-18 21:21:15 +0300

    Updated login page

--- front-end/src/pages/account/login/index.js
+++ front-end/src/pages/account/login/index.js
@@ -17,9 +17,11 @@ export default function LoginPage() {
   return (
     <>
       <MyContainer className="pt-[87px] text-black min-h-screen ">
-        <div className="grid h-[calc(100vh-87px)] place-items-center calc" >
-          <div className=" border border-gray-300 rounded-lg p-20 min-w-lg flex flex-col items-center">
-            <h1 className="text-3xl font-medium pb-5 text-left w-full">Login</h1>
+        <div className="grid h-[calc(100vh-87px)] place-items-center calc w-full px-4 lg:px-0">
+          <div className="flex flex-col gap-2 w-full max-w-[450px] min-[520px]:px-8 min-[520px]:py-12 sm:w-[450px] border border-gray-300 shadow px-4 py-8">
+            <h1 className="text-3xl font-medium pb-5 text-left w-full">
+              Login
+            </h1>
 
             <TextField
               label="Email"
@@ -35,16 +37,22 @@ export default function LoginPage() {
               sx={{ width: "100%", marginBottom: 4 }}
             />
 
-            <Link
-              className="w-full mx-auto"
-              href={"/dashboard"}
-              passHref
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <button class="rounded-md w-full bg-gray-800 text-slate-50 drop-shadow-lg py-2 px-6 uppercase tracking-wide text-sm tex -translate-y-2 ">
-                Login
-              </button>
-            </Link>
+            <div className=" flex flex-col gap-2 items-center">
+              <Link
+                className="w-full mx-auto"
+                href={"/dashboard"}
+                passHref
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <button class="rounded-md w-full bg-gray-800 text-slate-50 font-medium tracking-widest drop-shadow-lg py-3 px-6 uppercase text-base tex -translate-y-2 ">
+                  Login
+                </button>
+              </Link>
+              <small className=" text-sm text-center w-fit mx-auto">
+                Forgot your password?{" "}
+                <span className=" text-blue-500 underline">Click here.</span>
+              </small>
+            </div>
           </div>
         </div>
       </MyContainer>
