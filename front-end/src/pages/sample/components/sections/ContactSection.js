0eac6cc839d8f3bcc3050d73e14f57bbee91d569
Scott Baban <200431995@my.xu.edu.ph>
2023-05-19 03:07:10 +0800

    Updated ContactSection

--- front-end/src/pages/sample/components/sections/ContactSection.js
+++ front-end/src/pages/sample/components/sections/ContactSection.js
@@ -4,12 +4,14 @@ import MyContainer from "@modules/components/ui/MyContainer";
 import homeBG3 from "../../../../../public/images/gymbg3.png";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image from "next/Image";
 import gym1 from "../../../../../public/images/gym1.png";
 import gym2 from "../../../../../public/images/gym2.png";
 import gym3 from "../../../../../public/images/gym3.png";
 import gym4 from "../../../../../public/images/gym4.png";
 import styles from "../../../carousel.module.css";
-import Image from "next/image";
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import ContactPhoneRoundedIcon from '@mui/icons-material/ContactPhoneRounded';
 
 const ContactSection = ({ ...props }) => {
   return (
@@ -23,10 +25,8 @@ const ContactSection = ({ ...props }) => {
         backgroundPosition: "center",
       }}
     >
-      <MyContainer className="grid place-items-center md:h-[calc(100vh-74px)]">
-        <div
-          className={`${styles.carouselContainer} flex justify-center items-center`}
-        >
+      <MyContainer className="grid place-items-center h-[calc(100vh-74px)]">
+        <div className={`${styles.carouselContainer} flex justify-center items-center`}>
           <Carousel
             className={styles.gymImages}
             showThumbs={false}
@@ -53,6 +53,21 @@ const ContactSection = ({ ...props }) => {
               <Image src={gym4} />
             </div>
           </Carousel>
+        </div>
+        
+        <div className="bg-white p-4 rounded-lg mt-8 text-center">
+          <h2 className="text-3xl font-bold mb-2">Unscathed Fitness Gym</h2>
+          <p className="text-lg text-gray-500 mb-2">2nd Floor, Forever Books Bldg., Bulua Highway, Cagayan de Oro, Philippines</p>
+          <div className="flex items-center justify-center">
+            <FacebookRoundedIcon className="text-gray-500 mr-2" style={{ fontSize: "24px", marginBottom: "-2px" }} />
+            <a href="https://facebook.com/unscthdfitness" className="text-lg text-gray-500 hover:underline" target="_blank" rel="noopener noreferrer">
+              facebook.com/unscthdfitness
+            </a>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <ContactPhoneRoundedIcon className="text-gray-500 mr-2" style={{ fontSize: "24px", marginTop: "-1px" }} />
+            <p className="text-lg text-gray-500 mt-0">0905 920 8736</p>
+          </div>
         </div>
       </MyContainer>
     </Section>
