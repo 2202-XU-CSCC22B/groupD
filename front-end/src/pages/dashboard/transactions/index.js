6592dec413df1b040fc05865236bc8556617435c
NikkoWhoa <20170018421@my.xu.edu.ph>
2023-05-21 18:50:35 +0800

    next config

--- front-end/src/pages/dashboard/transactions/index.js
+++ front-end/src/pages/dashboard/transactions/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import AllMembersContent from "@modules/components/members/new/AllMembersContent";
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import { RiAddCircleLine } from 'react-icons/ri';
 
 const TransactionPage = () => {
   return (
@@ -9,10 +11,14 @@ const TransactionPage = () => {
       </Head>
 
       <div className="container pr-4 py-8 overflow-hidden">
-        <section className="">
-          <h1 className=" text-3xl font-bold xl:text-left xl:pl-4 text-gray-800">
-            All Transactions
-          </h1>
+        <section className="md:min-w-[720px] w-fit gap-8 flex justify-between items-center ">
+          <h1 className=" text-xl md:text-3xl font-bold xl:text-left xl:pl-4 text-gray-800 leading-none">
+            All Transactions 
+          </h1> <RiAddCircleLine size={30} className=" text-gray-800 cursor-pointer" />
+            
+
+
+
         </section>
         <AllMembersContent />
       </div>
