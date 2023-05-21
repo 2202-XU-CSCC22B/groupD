fd668f5780ab664e346598c12d0c612b474a1893
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-21 13:52:49 +0300

    Update next.config.js

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -4,13 +4,7 @@ module.exports = {
     companyName: "Unscathed Fitness Gym",
     duplicateEmailErrorMessage: "already exists!",
 
-<<<<<<< HEAD
-
-
     // // create_members_api : "http://localhost:8080/member/new",
-=======
-    // create_members_api : "http://localhost:8080/member/new",
->>>>>>> b8b7f0b5e73a2ac0dea5a7579977e8461510a239
     // retrieve_members_api : "http://localhost:8080/member/all",
     // retrieve_unverified_api: "http://localhost:8080/member/unverified",
     // update_member_api: "http://localhost:8080/member/update/{id}",
@@ -23,31 +17,6 @@ module.exports = {
     // update_staff_api: "localhost:8080/api/v1/staff/update/{id}",
     // retrieve_all_staff_api: "localhost:8080/api/v1/staff/all",
     // retrieve_staff_api: "localhost:8080/api/v1/staff/{id}",
-<<<<<<< HEAD
-
-
-
-    create_members_api:
-        "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/new",
-    retrieve_members_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/all",
-    retrieve_unverified_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/unverified",
-    update_member_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com/:8080member/update/{email}",
-    validate_unverified_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/validate/{email}",
-    delete_member_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/delete/{email}",
-    count_members_api:
-    "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/count",
-
-    create_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/new",
-    update_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/update/{id}",
-    retrieve_all_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/all",
-    retrieve_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/{id}",
-=======
->>>>>>> b8b7f0b5e73a2ac0dea5a7579977e8461510a239
 
     create_members_api:
       "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/new",
