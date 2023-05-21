392b6792dee6f923aad1ca44d40206deae667e3a
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-21 21:06:20 +0800

    completed the api end points

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -5,53 +5,51 @@ module.exports = {
     duplicateEmailErrorMessage: "already exists!",
 
 
-    create_members_api : "http://localhost:8080/member/new",
-    retrieve_members_api : "http://localhost:8080/member/all",
-    retrieve_unverified_api: "http://localhost:8080/member/unverified",
-    update_member_api: "http://localhost:8080/member/update/{id}",
-    validate_unverified_api: "http://localhost:8080/member/validate/{email}",
-    delete_member_api: "http://localhost:8080/member/delete/{email}",
 
 
 
-    create_members_api : "http://localhost:8080/member/new",
-    retrieve_members_api : "http://localhost:8080/member/all",
-    retrieve_unverified_api: "http://localhost:8080/member/unverified",
-    update_member_api: "http://localhost:8080/member/update/{id}",
-    validate_unverified_api: "http://localhost:8080/member/validate/{email}",
-    delete_member_api: "http://localhost:8080/member/delete/{email}",
-    count_members_api:
-    "http://localhost:8080/member/count",
+    // create_members_api : "http://localhost:8080/member/new",
+    // retrieve_members_api : "http://localhost:8080/member/all",
+    // retrieve_unverified_api: "http://localhost:8080/member/unverified",
+    // update_member_api: "http://localhost:8080/member/update/{id}",
+    // validate_unverified_api: "http://localhost:8080/member/validate/{email}",
+    // delete_member_api: "http://localhost:8080/member/delete/{email}",
+    // count_members_api: "http://localhost:8080/member/count",
+    // get_member_transaction_api: "localhost:8080/member/{id}/transactions",
+    //
+    //
+    // create_staff_api: "http://localhost:8080/api/v1/staff/new",
+    // get_staff_by_id_api: "localhost:8080/api/v1/staff/{id}",
+    // get_staff_transaction_api: "localhost:8080/api/v1/staff/{id}",
+    // update_staff_api: "http://localhost:8080/api/v1/staff/update/{id}",
+    // retrieve_all_staff_api: "http://localhost:8080/api/v1/staff/all",
+    // retrieve_staff_api: "http://localhost:8080/api/v1/staff/{id}",
+    //
+    // get_all_transaction_api: "localhost:8080/api/v1/transaction/all",
+    // get_transactions_by_month_api: "localhost:8080/api/v1/transaction/{month}",
+    // post_new_transaction_api: "localhost:8080/api/v1/transaction/new",
 
-    create_staff_api: "http://localhost:8080/api/v1/staff/new",
-    update_staff_api: "http://localhost:8080/api/v1/staff/update/{id}",
-    retrieve_all_staff_api: "http://localhost:8080/api/v1/staff/all",
-    retrieve_staff_api: "http://localhost:8080/api/v1/staff/{id}",
 
 
+    create_members_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/new",
+    retrieve_members_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/all",
+    retrieve_unverified_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/unverified",
+    update_member_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/update/{id}",
+    validate_unverified_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/validate/{email}",
+    delete_member_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/delete/{email}",
+    count_members_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/count",
+    get_member_transaction_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/{id}/transactions",
 
-    create_members_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/new",
-    retrieve_members_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/all",
-    retrieve_unverified_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/unverified",
-    update_member_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com/:8080member/update/{email}",
-    validate_unverified_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/validate/{email}",
-    delete_member_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/delete/{email}",
-    count_members_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/member/count",
+    create_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/new",
+    get_staff_by_id_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/{id}",
+    get_staff_transaction_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/{id}",
+    update_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/update/{id}",
+    retrieve_all_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/all",
+    retrieve_staff_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/{id}",
 
-    create_staff_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/new",
-    update_staff_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/update/{id}",
-    retrieve_all_staff_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/all",
-    retrieve_staff_api:
-      "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/staff/{id}",
+    get_all_transaction_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/transaction/all",
+    get_transactions_by_month_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/transaction/{month}",
+    post_new_transaction_api: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8080/api/v1/transaction/new"
+
   },
 };
