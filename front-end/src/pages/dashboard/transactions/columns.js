9df228448ebf59bc45a84633519417077422c6de
NikkoWhoa <128227417+NikkoWhoa@users.noreply.github.com>
2023-05-22 04:07:39 +0300

    Added transactions table
    - Added reusable table using TanStack Table
    - added transactions table

---
+++ front-end/src/pages/dashboard/transactions/columns.js
@@ -0,0 +1,120 @@
+import { TbArrowsDownUp } from "react-icons/tb";
+
+export const columns = [
+  {
+    accessorKey: "transactionType",
+    header: ({ column }) => {
+      console.log(column);
+      return (
+        <div className=" flex items-center gap-1">
+          <p>Type</p>
+          <div>
+            <select
+              defaultValue={column.getFilterValue() ?? ""}
+              onChange={(event) => column.setFilterValue(event.target.value)}
+              className="outline-none w-[16px] bg-inherit cursor-pointer"
+            >
+              <option hidden></option>
+              <option value="">All</option>
+              <option value="sales">Sales</option>
+              <option value="cash out">Cash Out</option>
+              <option value="Salary">Salary</option>
+              <option value="maintenance">Maintenance</option>
+              <option value="utilities">Utilities</option>
+            </select>
+          </div>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: ({ column }) => {
+      console.log(column);
+      return (
+        <div className=" flex items-center gap-1">
+          <p>Method</p>
+          <div>
+            <select
+              defaultValue={column.getFilterValue() ?? ""}
+              onChange={(event) => column.setFilterValue(event.target.value)}
+              className="outline-none w-[16px] bg-inherit cursor-pointer"
+            >
+              <option hidden></option>
+              <option value="">All</option>
+              <option value="GC">GCash</option>
+              <option value="CASH">Cash</option>
+            </select>
+          </div>
+        </div>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <div>{row.original.paymentMethod === "GC" ? "GCash" : "Cash"}</div>
+      );
+    },
+  },
+
+  {
+    accessorKey: "firstName",
+    header: ({ column }) => {
+      return (
+        <div className=" flex items-center gap-1">
+          <p>First name</p>
+          <TbArrowsDownUp
+            className=" cursor-pointer"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "lastName",
+    header: ({ column }) => {
+      return (
+        <div className=" flex items-center gap-1">
+          <p>Last name</p>
+          <TbArrowsDownUp
+            className="cursor-pointer"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+    cell: ({ row }) => {
+      return (
+        <div className="">
+          {row.original.description === null ? "--" : row.original.description}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "memberID",
+    header: "Member ID",
+    cell: ({ row }) => {
+      return (
+        <div className="">
+          {row.original.memberID === null ? "--" : row.original.memberID}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "staffID",
+    header: "Staff ID",
+    cell: ({ row }) => {
+      return (
+        <div className="">
+          {row.original.staffID === null ? "--" : row.original.staffID}
+        </div>
+      );
+    },
+  },
+];
