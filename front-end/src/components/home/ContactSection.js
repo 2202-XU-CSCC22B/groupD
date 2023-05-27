206cad359e17cb89b5ff326522d1e25e45f51a33
Scott Baban <200431995@my.xu.edu.ph>
2023-05-28 01:11:39 +0800

    Updated ContactSection and LoginPage.

--- front-end/src/components/home/ContactSection.js
+++ front-end/src/components/home/ContactSection.js
@@ -15,78 +15,82 @@ import Image from "next/image";
 
 const ContactSection = ({ ...props }) => {
   return (
-    <Section
-      {...props}
-      className="flex items-center -z-50"
-      style={{
-        backgroundImage: `url(${homeBG3.src})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <MyContainer className="grid place-items-center h-[calc(100vh-74px)]">
-        <div
-          className={`${styles.carouselContainer} flex justify-center items-center`}
-        >
-          <Carousel
-            className={styles.gymImages}
-            showThumbs={false}
-            infiniteLoop={true}
-            centerMode={false}
-            showArrows={true}
-            emulateTouch={true}
-            autoPlay={true}
-            interval={3000}
-            stopOnHover={true}
-            swipeScrollTolerance={10}
-            dynamicHeight={false}
+    <div style={{ padding: "20px" }}>
+      <Section
+        {...props}
+        className="flex items-center -z-50"
+        style={{
+          backgroundImage: `url(${homeBG3.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <MyContainer className="grid place-items-center h-[calc(100vh-74px)]">
+          <div
+            className={`${styles.carouselContainer} flex justify-center items-center`}
+            style={{ maxWidth: "100%", width: "100%" }}
           >
-            <div>
-              <Image src={gym1} />
-            </div>
-            <div>
-              <Image src={gym2} />
-            </div>
-            <div>
-              <Image src={gym3} />
-            </div>
-            <div>
-              <Image src={gym4} />
-            </div>
-          </Carousel>
-        </div>
-
-        <div className="bg-white p-4 rounded-lg mt-8 text-center">
-          <h2 className="text-3xl font-bold mb-2">Unscathed Fitness Gym</h2>
-          <p className="text-lg text-gray-500 mb-2">
-            2nd Floor, Forever Books Bldg., Bulua Highway, Cagayan de Oro,
-            Philippines
-          </p>
-          <div className="flex items-center justify-center">
-            <FacebookRoundedIcon
-              className="text-gray-500 mr-2"
-              style={{ fontSize: "24px", marginBottom: "-2px" }}
-            />
-            <a
-              href="https://facebook.com/unscthdfitness"
-              className="text-lg text-gray-500 hover:underline"
-              target="_blank"
-              rel="noopener noreferrer"
+            <Carousel
+              className={styles.gymImages}
+              showThumbs={false}
+              infiniteLoop={true}
+              centerMode={false}
+              showArrows={true}
+              emulateTouch={true}
+              autoPlay={true}
+              interval={3000}
+              stopOnHover={true}
+              swipeScrollTolerance={10}
+              dynamicHeight={false}
+              style={{ maxWidth: "100%", width: "100%" }}
             >
-              facebook.com/unscthdfitness
-            </a>
+              <div>
+                <Image src={gym1} />
+              </div>
+              <div>
+                <Image src={gym2} />
+              </div>
+              <div>
+                <Image src={gym3} />
+              </div>
+              <div>
+                <Image src={gym4} />
+              </div>
+            </Carousel>
           </div>
-          <div className="flex items-center justify-center mt-2">
-            <ContactPhoneRoundedIcon
-              className="text-gray-500 mr-2"
-              style={{ fontSize: "24px", marginTop: "-1px" }}
-            />
-            <p className="text-lg text-gray-500 mt-0">0905 920 8736</p>
+
+          <div className="bg-white p-4 rounded-lg mt-8 text-center">
+            <h2 className="text-3xl font-bold mb-2">Unscathed Fitness Gym</h2>
+            <p className="text-lg text-gray-500 mb-2">
+              2nd Floor, Forever Books Bldg., Bulua Highway, Cagayan de Oro,
+              Philippines
+            </p>
+            <div className="flex items-center justify-center">
+              <FacebookRoundedIcon
+                className="text-gray-500 mr-2"
+                style={{ fontSize: "24px", marginBottom: "-2px" }}
+              />
+              <a
+                href="https://facebook.com/unscthdfitness"
+                className="text-lg text-gray-500 hover:underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                facebook.com/unscthdfitness
+              </a>
+            </div>
+            <div className="flex items-center justify-center mt-2">
+              <ContactPhoneRoundedIcon
+                className="text-gray-500 mr-2"
+                style={{ fontSize: "24px", marginTop: "-1px" }}
+              />
+              <p className="text-lg text-gray-500 mt-0">0905 920 8736</p>
+            </div>
           </div>
-        </div>
-      </MyContainer>
-    </Section>
+        </MyContainer>
+      </Section>
+    </div>
   );
 };
 
--- front-end/src/pages/account/login/index.js
+++ front-end/src/pages/account/login/index.js
@@ -65,7 +65,7 @@ export default function LoginPage() {
         <div className="grid h-[calc(100vh-87px)] place-items-center calc w-full px-4 lg:px-0">
           <div className="flex flex-col gap-2 w-full max-w-[450px] min-[520px]:px-8 min-[520px]:py-12 sm:w-[450px] border border-gray-300 shadow px-4 py-8 bg-white rounded-lg">
             <h1 className="text-3xl font-medium pb-5 text-left w-full">
-              Login
+              Admin Login
             </h1>
 
             <TextField
